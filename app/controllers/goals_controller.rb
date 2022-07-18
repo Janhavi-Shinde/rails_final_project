a283b2d1e77ee1c0b2a46caf9aac1686be543351
janhavi <janashinde394@gmail.com>
2022-07-18 20:00:01 +1000

    added goal#create including checkbox option to select sphere, and goals now properly associated with join table, updated sphere's and goal's show pages

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -6,7 +6,15 @@ class GoalsController < ApplicationController
     def create
         @goal = Goal.new(goals_params)
         @goal.save
-        redirect_to spheres_path
+
+        # sphere_goal_instance = SpheresGoal.find_by(goal_id: @goal.id)
+        # sphere_id = sphere_goal_instance.sphere_id
+        
+        # sphere_id = get_sphere(@goal)
+        # redirect_to sphere_path(sphere_id) 
+         redirect_to goal_path(@goal)
+
+        
         # what path do we pass here for this urlhelper? 
         # we have many spheres (through spheres_goals),
         # maybe we can create a model method that gets the sphere id for the specific goal id?(we'll have to pass the current goal id in as an argument)
@@ -16,10 +24,13 @@ class GoalsController < ApplicationController
         # sphere_goal_var.sphere_id
     end
 
-    
+    def show
+        @goal = Goal.find(params[:id])
+
+    end
     private
     def goals_params
-        params.require(:goal).permit(:title, :description)
+        params.require(:goal).permit(:title, :description, sphere_ids:[])
     end
 
 end
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -4,8 +4,8 @@ class Goal < ApplicationRecord
     has_many :spheres, through: :spheres_goals
 
     def get_sphere(current_goal_id)
-        # sphere_goal_var = goal.sphere_goal.where(goal_id == current_goal_id )
-        # sphere_goal_var.sphere_id
+        sphere_goal_var = current_goal_id.spheres_goal.where(goal_id == current_goal_id )
+        sphere_goal_var.sphere_id
         
     end
 end
