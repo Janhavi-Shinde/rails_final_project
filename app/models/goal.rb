e2fd2dad9818f43120e55cf4abb4a75dca3ca6c1
janhavi <janashinde394@gmail.com>
2022-07-21 21:20:43 +1000

    added basic validation action to Goal and Sphere model

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -3,6 +3,10 @@ class Goal < ApplicationRecord
     has_many :spheres_goals
     has_many :spheres, through: :spheres_goals
 
+    validates :title, presence: true
+    validates :description, length: {maximum: 100}
+    validates :sphere_ids, presence: true
+
     def get_sphere(current_goal_id)
         sphere_goal_var = current_goal_id.spheres_goal.where(goal_id == current_goal_id )
         sphere_goal_var.sphere_id
--- app/models/sphere.rb
+++ app/models/sphere.rb
@@ -2,4 +2,6 @@ class Sphere < ApplicationRecord
     has_many :spheres_goals
     has_many :goals, through: :spheres_goals
     belongs_to :user
+
+    validates :title, presence: true
 end
