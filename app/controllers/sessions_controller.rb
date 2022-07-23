dc2d83b20b149a8f28259fec9ba35181363c0c0b
janhavi <janashinde394@gmail.com>
2022-07-24 00:15:19 +1000

    added basic new user form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < ApplicationController
     def new
-
-        
     end
+    
     def create
 
         user = User.find_by(email: params[:email])
+        
             if user && user.password == params[:password]
                 session[:user_id] = user.id
                 redirect_to user_path(user)
