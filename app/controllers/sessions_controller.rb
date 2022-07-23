759700c680ff00957133204d53fb8d04b5466f45
janhavi <janashinde394@gmail.com>
2022-07-24 00:07:16 +1000

    added authentication (login and log out using sessions)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,32 @@
 class SessionsController < ApplicationController
     def new
+
         
     end
     def create
-        return redirect_to(controller: 'sessions',
-                       action: 'new') if !params[:username] || params[:name].empty?
 
-    session[:name] = params[:name]
-    redirect_to controller: 'user', action: 'show'
+        user = User.find_by(email: params[:email])
+            if user && user.password == params[:password]
+                session[:user_id] = user.id
+                redirect_to user_path(user)
+            else
+                flash.now[:notice] = "Invalid email or password. Try again if ur not a hacker"
+                 render :new
+            end 
+
     
     end
+
+    def show
+        if User.find_by(username: params[:name]) 
+        user = User.find_by(username: params[:name]) 
+        session[:user_id] = user.id
+        end
+    end
+
+    def destroy
+        session.delete :email
+        session.delete :password
+        redirect_to '/'
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  # root 'sessions#new'
+  root 'sessions#new'
   resources :spheres_goals
+  
+  # get '/login', to: 'users#'
   resources :goals
   resources :spheres
   resources :users do
     resources :spheres 
   end
+  resources :sessions, only: [:new, :create, :show]
+  delete '/signout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
